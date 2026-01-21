14cde9e68c0885b4ee50954a784bef7a869b4187
Samuel Ollor <s.g.o@live.com>
2026-01-21 06:28:08 +0100

    Add FPS limiting and delta time using pygame clock

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         log_state()
 
@@ -17,6 +20,8 @@ def main():
 
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.get_time() / 1000
 
 
 if __name__ == "__main__":
