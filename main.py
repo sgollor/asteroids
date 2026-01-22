427aefe157228e4039164edb424d7839f95484d7
Samuel Ollor <s.g.o@live.com>
2026-01-22 19:49:24 +0100

    use sprite groups for player update and draw

--- main.py
+++ main.py
@@ -6,12 +6,18 @@ from player import Player
 def main():
     print("Starting Asteroids with pygame version: VERSION")
     print(f"Screen width: {SCREEN_WIDTH} Screen height: {SCREEN_HEIGHT}")
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     clock = pygame.time.Clock()
     dt = 0
     
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
@@ -21,9 +27,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
         screen.fill((0, 0, 0))
-        player.draw(screen)
+
+        updatable.update(dt)
+
+        for drawable_obj in drawable:
+            drawable_obj.draw(screen)
+
         pygame.display.flip()
         clock.tick(60)
         dt = clock.get_time() / 1000
