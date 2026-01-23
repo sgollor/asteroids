2d5b4000581acdeb728b286cf05a2652e4a3d83e
Samuel Ollor <s.g.o@live.com>
2026-01-23 15:06:55 +0100

    add shooting and shot entity

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     print("Starting Asteroids with pygame version: VERSION")
@@ -19,15 +20,17 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
-
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     Asteroid.containers = (asteroids, updatable, drawable)
 
     AsteroidField.containers = (updatable,)
     asteroid_field = AsteroidField()
+
+    Shot.containers = (shots, updatable, drawable)
 
     while True:
         log_state()
